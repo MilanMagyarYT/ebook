c079f0108c2c26af0faef16ac641471e33446fa5
MilanMagyarYT <130912856+MilanMagyarYT@users.noreply.github.com>
2025-07-12 01:02:50 +0200

    aaaa

--- src/pages/Cancel.tsx
+++ src/pages/Cancel.tsx
@@ -5,24 +5,27 @@ export default function Cancel() {
   const retry = () => window.location.replace("/");
 
   return (
-    <section className="checkout-wrapper cancel">
+    <div>
       <img src={Logo} alt="Logo" className="hero-logo" />
 
-      <div className="icon">❌</div>
-      <h1>Payment cancelled</h1>
+      <section className="checkout-wrapper cancel">
+        <div className="icon">❌</div>
+        <h1>Payment cancelled</h1>
 
-      <p className="lead">
-        No worries—your card hasn’t been charged. You can try again or reach out
-        if you’re having trouble.
-      </p>
+        <p className="lead">
+          No worries—your card hasn’t been charged. You can try again or reach
+          out if you’re having trouble.
+        </p>
 
-      <button className="primary-btn" onClick={retry}>
-        Try again
-      </button>
+        <button className="primary-btn" onClick={retry}>
+          Try again
+        </button>
 
-      <p className="meta">
-        Questions? <a href="mailto:findstudenthousingnl@gmail.com">E-mail us</a>
-      </p>
-    </section>
+        <p className="meta">
+          Questions?{" "}
+          <a href="mailto:findstudenthousingnl@gmail.com">E-mail us</a>
+        </p>
+      </section>
+    </div>
   );
 }
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -3,22 +3,23 @@ import Logo from "../assets/logo.png";
 
 export default function Success() {
   return (
-    <section className="checkout-wrapper success">
+    <div>
       <img src={Logo} alt="Logo" className="hero-logo" />
+      <section className="checkout-wrapper success">
+        <div className="icon">✅</div>
+        <h1>Thank you for your purchase!</h1>
 
-      <div className="icon">✅</div>
-      <h1>Thank you for your purchase!</h1>
+        <p className="lead">
+          Your Student Housing Guide is being sent to your inbox right now.
+        </p>
 
-      <p className="lead">
-        Your Student Housing Guide is being sent to your inbox right now.
-      </p>
-
-      <div className="order-info">
-        <span>
-          Need help?{" "}
-          <a href="mailto:findstudenthousingnl@gmail.com">Contact us</a>
-        </span>
-      </div>
-    </section>
+        <div className="order-info">
+          <span>
+            Need help?{" "}
+            <a href="mailto:findstudenthousingnl@gmail.com">Contact us</a>
+          </span>
+        </div>
+      </section>
+    </div>
   );
 }
