50611c6c820277d6811effe6bb5bd77308620ddd
MilanMagyarYT <130912856+MilanMagyarYT@users.noreply.github.com>
2025-07-12 00:51:02 +0200

    aaaa

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,23 +1,19 @@
 import "./CheckoutPages.css"; // styles below
+import Logo from "../../assets/logo.png";
 
 export default function Success() {
-  const orderNo =
-    new URLSearchParams(window.location.search).get("order") ?? "N/A";
-  const email =
-    new URLSearchParams(window.location.search).get("email") ?? "your inbox";
-
   return (
     <section className="checkout-wrapper success">
+      <img src={Logo} alt="Logo" className="hero-logo" />
+
       <div className="icon">✅</div>
       <h1>Thank you for your purchase!</h1>
 
       <p className="lead">
-        Your Student Housing Guide is being sent to <strong>{email}</strong>{" "}
-        right now.
+        Your Student Housing Guide is being sent to your inbox right now.
       </p>
 
       <div className="order-info">
-        <span>Order&nbsp;#{orderNo}</span>
         <span>
           Need help?{" "}
           <a href="mailto:findstudenthousingnl@gmail.com">Contact us</a>
